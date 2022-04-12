78e7ac89071f2982e6f45fa5e0b0b5084bc4e025
alexsandroluz <alexsandroluz@lampp-it.com.br>
2022-04-12 15:01:58 -0300

    Create Calculator sub

---
+++ 01_RestWhithASPNET_ScaffodViaVisualStudio/RestWhite.Net/RestWhite.Net/Controllers/CalculatorSub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace RestWhite.Net.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorSub : Controller
+    {
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sub.ToString());
+            }
+            return BadRequest("invalid input");
+        }
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any,
+                System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+
+        private double ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return (double)decimalValue;
+            }
+            return 0;
+        }
+    }
+}
